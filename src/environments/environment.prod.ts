9d7ec8034ac83c132844980f13f6c25283383075
SavanJ <30324249+SavanJ@users.noreply.github.com>
2019-12-28 09:24:05 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase:{
-  	apiKey: "AIzaSyBxwGIkgb8QGp9JYX-REzlwrNq30TEpw7Y",
-    authDomain: "newshop-8deb3.firebaseapp.com",
-    databaseURL: "https://newshop-8deb3.firebaseio.com",
-    projectId: "newshop-8deb3",
-    storageBucket: "newshop-8deb3.appspot.com",
-    messagingSenderId: "16123372574",
-    appId: "1:16123372574:web:d1a9e05f953674a5bbba0f",
-    measurementId: "G-83B678GF21"
+  	apiKey: 'YOUR API KEY',
+    authDomain: "YOUR FIREBASE NAME",
+    databaseURL: "URL",
+    projectId: "PROJECT ID",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
